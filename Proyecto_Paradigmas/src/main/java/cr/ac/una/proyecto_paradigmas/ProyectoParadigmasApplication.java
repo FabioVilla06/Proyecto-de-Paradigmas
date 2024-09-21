2826b9f14d48f4b5e49774dfbed57e30aa27983a
FabioVilla06 <fabio.villalobos.pacheco@est.una.ac.cr>
2024-09-20 18:39:24 -0600

    se agrega proyecto

---
+++ Proyecto_Paradigmas/src/main/java/cr/ac/una/proyecto_paradigmas/ProyectoParadigmasApplication.java
@@ -0,0 +1,13 @@
+package cr.ac.una.proyecto_paradigmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoParadigmasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoParadigmasApplication.class, args);
+    }
+
+}
---
+++ Proyecto_Paradigmas/src/test/java/cr/ac/una/proyecto_paradigmas/ProyectoParadigmasApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.ac.una.proyecto_paradigmas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoParadigmasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
